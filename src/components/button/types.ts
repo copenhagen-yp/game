e15cca8c1b9779ec14dda24f76b483d6f5da07e7
Aleksandr Kochura <kochura@gmail.com>
2020-12-25 00:41:01 +0300

    вернул тип для disabled

--- src/components/button/types.ts
+++ src/components/button/types.ts
@@ -4,6 +4,7 @@ export type Props = {
     className?: string;
     tagType?: 'link' | 'button';
     type?: 'submit' | 'button' | 'reset';
+    disabled?: boolean;
     onClick: (e: React.MouseEvent) => void;
     children: React.ReactNode;
 };
