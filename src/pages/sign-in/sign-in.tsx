c100bdbf20e9d91dbea59214fc4f6a32727c3e5c
Mar1nka <sakovich.mandarina@gmail.com>
2020-12-25 17:47:23 +0300

    declare module '*.pcss', поменять типы для для route-wrapper

--- src/pages/sign-in/sign-in.tsx
+++ src/pages/sign-in/sign-in.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// @ts-ignore
 import styles from './sign-in.pcss';
 
 export const SignIn = () => {
--- src/route-wrapper/types.ts
+++ src/route-wrapper/types.ts
@@ -1,5 +1,8 @@
+import { ComponentType } from 'react';
+import { RouteProps } from "react-router-dom";
+
 export type RouteWrapperType = {
-  component: any, // ToDo: change type
-  layout: any, // ToDo: change type
+  component: ComponentType<RouteProps>,
+  layout: ComponentType<RouteProps>,
   path: string,
 }
