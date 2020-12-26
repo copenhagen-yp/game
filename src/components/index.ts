b534efd3b691ddfde08a40f48dac2774fa98e9e9
Anatolii Bartsev <a.barczev@yandex.ru>
2020-12-26 20:56:48 +0300

    добавил компонент инпут

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import { Button } from './button';
 import { Toast } from './toast';
+import { Input } from './input';
 
 export {
   Button,
   Toast,
+  Input
 };
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: true
             },
           },
           'postcss-loader'
