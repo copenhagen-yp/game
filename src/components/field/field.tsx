87332aa4ff1a5684979eac4c4f116c0e644f16e8
Anatolii Bartsev <a.barczev@yandex.ru>
2020-12-27 14:20:44 +0300

    поправил пропсы

--- src/components/field/field.tsx
+++ src/components/field/field.tsx
@@ -4,12 +4,13 @@ import { Props } from './types';
 import styles from './field.pcss';
 
 export const Field: FC<Props> = (props) => {
-    const {children, label, error, disabled = true} = props;
+    const {children, label, isError} = props;
+    
     return (
         <div className={styles.field}>
             <label className={styles.field__label}>{label}</label>
             {children}
-            {disabled ? <span className={styles.error}>{error || 'Поле обязательно для заполнения'}</span> : null}
+            {isError ? <span className={styles.error}>Поле обязательно для заполнения</span> : null}
         </div>
     )
 };
--- src/components/field/types.ts
+++ src/components/field/types.ts
@@ -3,6 +3,5 @@ import { ReactNode } from 'react';
 export type Props = {
     children: ReactNode,
     label?: string,
-    error?: string,
-    disabled?: boolean
+    isError?: boolean
 };
