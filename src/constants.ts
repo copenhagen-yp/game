2d01df8fe14ed69d97a74a6830e2b80bde357d79
Anatolii Bartsev <a.barczev@yandex.ru>
2021-01-08 18:51:12 +0300

    вернул условие еслинт

--- src/constants.ts
+++ src/constants.ts
@@ -4,15 +4,15 @@ export enum APP_TEXT {
   REGISTRATION_BTN_TEXT = 'Зарегистрироваться',
   AUTH_TEXT = 'Авторизоваться',
   ERROR_EMAIL = 'Не корректный email'
-};
+}
 
 export enum API_URL {
   DOMAIN = 'https://ya-praktikum.tech/api/v2/',
   SIGN_IN = 'auth/signin',
   LEADERBOARD_ALL = 'leaderboard/all',
   SIGN_UP = '/auth/signup'
-};
+}
 
 export const APP_REGULAR = {
-  VALID_EMAIL: /^([\w-\.*]+@([\w-]+\.)+[\w-]{2,4})?$/
+  VALID_EMAIL: /^([\w-.*]+@([\w-]+\.)+[\w-]{2,4})?$/
 };
