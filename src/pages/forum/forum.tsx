ab68fdf3cfa1d52a0b88d7bda5b84cc33301fa0e
Aleksandr Kochura <kochura@gmail.com>
2021-01-12 00:57:42 +0300

    фиксированный интервал движений

--- src/pages/forum/forum.tsx
+++ src/pages/forum/forum.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { useForum, useForm, useHttp } from '../../hooks';
-import { APP_TEXT } from '../../constants';
+import { APP_TEXT, REQUEST_METHOD } from '../../constants';
 import { routes } from '../../routes';
 import { Field, Input, Button } from '../../components';
 import styles from './forum.pcss';
 
 export const Forum = () => {
   const requiredFields = ['question_name', 'question_description'];
-  const { request } = useHttp('');
+  const { request } = useHttp('', REQUEST_METHOD.POST);
   const { handleChange, handleBlur, fields, error } = useForm(request, requiredFields);
-  const { forums, handleSubmitQuestion } = useForum(fields);  
+  const { forums, handleSubmitQuestion } = useForum(fields);
 
   return (
     <div className={styles.container}>
@@ -32,16 +32,16 @@ export const Forum = () => {
       <form onSubmit={handleSubmitQuestion} className={styles.forum__form}>
         <h3>{APP_TEXT.FORUM_TITLE}</h3>
         <Field label="Тема" error={error.name}>
-          <Input 
-            name="name" 
+          <Input
+            name="name"
             type="text"
             error={error.name}
             onChange={handleChange}
             onBlur={handleBlur} />
         </Field>
         <Field label="Описание" error={error.description}>
-          <Input 
-            name="description" 
+          <Input
+            name="description"
             type="text"
             error={error.description}
             onChange={handleChange}
--- src/pages/game/play-ground/play-ground.ts
+++ src/pages/game/play-ground/play-ground.ts
@@ -1,15 +1,22 @@
 import { MainCharacter } from '../main-character';
 
+const INTERVAL_MOTION = 1 / 60;
+
 export class PlayGround {
   private canvas: any | null;
   private context: any | null;
 
   private mainCharacter: any | null;
   private requestAnimationId: any | undefined;
+  private lastTime: number;
+  private timeDelta: number;
 
   constructor(canvas: any, context: any) {
     this.canvas = canvas;
     this.context = context;
+
+    this.lastTime = 0;
+    this.timeDelta = 0;
 
     this.mainCharacter = null;
     this.requestAnimationId = undefined;
@@ -24,6 +31,7 @@ export class PlayGround {
     this.initCanvas();
     this.initMainCharacter();
 
+    this.lastTime = performance.now();
     this.requestAnimationId = requestAnimationFrame(this.render);
   }
 
@@ -60,11 +68,19 @@ export class PlayGround {
   }
 
   render = () => {
+    const now = performance.now();
+
+    this.timeDelta += (now - this.lastTime) / 1000;
+
+    while (this.timeDelta > INTERVAL_MOTION) {
+      this.timeDelta -= INTERVAL_MOTION;
+      this.mainCharacter.move();
+    }
+
+    this.lastTime = now;
+
     this.clearCanvas();
-
-    this.mainCharacter.move();
     this.mainCharacter.draw();
-
     this.requestAnimationId = requestAnimationFrame(this.render);
   }
 }
