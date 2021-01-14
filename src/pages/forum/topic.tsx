53ae87e1aa977ee5f56ca4d536d65f6ca4cd0e2c
Aleksandr <36853157+Sserpske@users.noreply.github.com>
2021-01-14 14:20:04 +0300

    фиксированный интервал движений (#19)

--- src/pages/forum/topic.tsx
+++ src/pages/forum/topic.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 import { useForum, useHttp, useForm } from '../../hooks';
 import { Input, Button, Field } from '../../components';
-import { APP_TEXT } from '../../constants';
+import { APP_TEXT, REQUEST_METHOD } from '../../constants';
 import styles from './forum.pcss';
 
 export const Topic = () => {
   const requiredFields = ['comment'];
-  const { request } = useHttp('');
+  const { request } = useHttp('', REQUEST_METHOD.POST);
   const { handleChange, handleBlur, fields, error } = useForm(request, requiredFields);
   const { currentForum, currentComments, handleSubmitComments } = useForum(fields);
-  
+
   return (
     <div className={styles.container}>
       <h3>{currentForum?.name}</h3>
@@ -25,8 +25,8 @@ export const Topic = () => {
       <form onSubmit={handleSubmitComments} className={styles.forum__form}>
         <h3>{APP_TEXT.COMMENT_TITLE}</h3>
         <Field label="Комментарий" error={error.comment}>
-          <Input 
-            name="comment" 
+          <Input
+            name="comment"
             type="text"
             error={error.comment}
             onChange={handleChange}
