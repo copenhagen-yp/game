73402d31d5fb07eceea3dea11e9919c3001e1b28
Aleksandr Kochura <kochura@gmail.com>
2021-02-26 00:30:48 +0300

    ...

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,15 @@ import compression from 'compression';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import { serverRenderMiddleware } from './server-render-middleware';
+import mongoose from 'mongoose';
+import configDB from '../database';
 
 const app = express();
+
+mongoose.connect(configDB.url, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 app.use(cookieParser());
 
