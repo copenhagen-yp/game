ba104480489c3565afccea7081a6a63607cda36f
Aleksandr Kochura <kochura@gmail.com>
2021-02-27 16:59:36 +0300

    .

--- src/app/test-mongo-data.ts
+++ src/app/test-mongo-data.ts
@@ -1,50 +1,40 @@
+// Тут просто сетятся тестовые данные,
+// чтобы понимать, чтобы БД работает.
+
 import mongoose from 'mongoose';
 
-import { Author } from 'app/models/author';
-import { Book } from 'app/models/book';
+import { User } from 'app/models/user';
+import { Feedback } from 'app/models/feedback';
 
 export const testMongoDb = () => {
-  const jamieAuthor = new Author({
+  const user = new User({
     _id: new mongoose.Types.ObjectId(),
     name: {
       firstName: 'Sasha',
-      lastName: 'Jo'
+      lastName: 'Ko',
     },
-    biography: 'Jamie is the author of ASP.NET MVC 5 with Bootstrap and Knockout.js.',
-    twitter: 'https://twitter.com/endyourif',
-    facebook: 'https://www.facebook.com/End-Your-If-194251957252562/'
   });
 
-  jamieAuthor.save(function(err) {
-    if (err) throw err;
+  user.save(function(err) {
+    if (err) {
+      throw err;
+    }
 
-    console.log('Author successfully saved.');
+    console.log('User successfully saved.');
 
-    const mvcBook = new Book({
+    const comment = new Feedback({
       _id: new mongoose.Types.ObjectId(),
-      title: 'ASP.NET MVC 5 with Bootstrap and Knockout.js',
-      author: jamieAuthor._id,
-      ratings:[{
-        summary: 'Great read'
-      }]
+      title: '5+',
+      body: 'Great game!',
+      user: user._id,
     });
 
-    mvcBook.save(function(err) {
-      if (err) throw err;
+    comment.save(function(err) {
+      if (err) {
+        throw err;
+      }
 
-      console.log('Book successfully saved.');
-    });
-
-    const knockoutBook = new Book({
-      _id: new mongoose.Types.ObjectId(),
-      title: 'Knockout.js: Building Dynamic Client-Side Web Applications',
-      author: jamieAuthor._id
-    });
-
-    knockoutBook.save(function(err) {
-      if (err) throw err;
-
-      console.log('Book successfully saved.');
+      console.log('Feedback successfully saved.');
     });
   });
 };
