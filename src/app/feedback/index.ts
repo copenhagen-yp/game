1509c76b6b4cb891a5f415bf37020b802e343c93
Aleksandr Kochura <kochura@gmail.com>
2021-03-03 00:39:38 +0300

    добавил ручку обратной связи

--- src/app/feedback/index.ts
+++ src/app/feedback/index.ts
@@ -1,15 +1,50 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+import { User, Feedback } from '../models';
+import mongoose from 'mongoose';
+
 const router = express.Router();
 
 router.use(bodyParser.json());
 
 router.post('/feedback', (req, res) => {
-  console.log(req.body);
+  if (!req.body.body) {
+    res.statusCode = 400;
+    res.send({ 'error': 'The feedback field is required' });
+
+    return;
+  }
+
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    name: {
+      firstName: req.body.first_name || 'unknown',
+      lastName: req.body.last_name || 'unknown',
+    },
+  });
+
+  user.save(function(err) {
+    if (err) {
+      throw err;
+    }
+
+    const comment = new Feedback({
+      _id: new mongoose.Types.ObjectId(),
+      title: req.body.title || 'unknown',
+      body: req.body.body,
+      user: user._id,
+    });
+
+    comment.save(function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  });
 
   res.json({
-    message: 'some answer'
+    message: 'Ok'
   });
 });
 
