08e9247d551b106c0418b032c940f04a9f37ca47
Anatolii Bartsev <a.barczev@yandex.ru>
2021-03-06 16:48:55 +0300

    добавил поле в форму

--- src/pages/feedback/feedback.tsx
+++ src/pages/feedback/feedback.tsx
@@ -2,23 +2,16 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/withStyles';
 
 import { APP_TEXT, API_URL } from '../../constants';
-import { Field, Form, Textarea } from '../../components';
+import { Field, Form, Input, Textarea } from '../../components';
 import { useForm } from '../../hooks';
 
 import styles from '../sign-in-up.pcss';
 
-const signInFields = [
-  {
-    name: 'massage',
-    label: 'Сообщение',
-  },
-];
-
 export const Feedback = withStyles(styles)(() => {
-  const requiredFields = ['massage'];
+  const requiredFields = ['name', 'massage'];
 
   const {
-    handleSubmitSign,
+    handleSubmit,
     handleChange,
     handleBlur,
     error,
@@ -29,25 +22,34 @@ export const Feedback = withStyles(styles)(() => {
     <div className={styles.page}>
       <div className={styles.content}>
         <Form
-          onSubmit={handleSubmitSign}
+          onSubmit={handleSubmit}
           submitButtonText={APP_TEXT.BTN_SEND}
           title={'Обратная связь'}
         >
-          {signInFields.map((field) => (
-            <Field
-              key={field.name}
-              label={field.label}
-              error={error[field.name]}
-            >
-              <Textarea
-                name={field.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={error[field.name]}
-                value={fieldsValues[field.name] || ''}
-              />
-            </Field>
-          ))}
+          <Field
+            label={'Имя'}
+            error={error['name']}
+          >
+            <Input
+              name={'name'}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={error['name']}
+              value={fieldsValues['name'] || ''}
+            />
+          </Field>
+          <Field
+            label={'Сообщение'}
+            error={error['massage']}
+          >
+            <Textarea
+              name={'massage'}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={error['massage']}
+              value={fieldsValues['massage'] || ''}
+            />
+          </Field>
         </Form>
       </div>
     </div>
