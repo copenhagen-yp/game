98cb8879a72f67c8deb95c2f0cd61a40fa8afae2
Aleksandr Kochura <kochura@gmail.com>
2021-03-13 16:28:50 +0300

    добавил абстрактные ручки для форума

--- src/app/forum/index.ts
+++ src/app/forum/index.ts
@@ -1,37 +1,124 @@
+// Контроллер с предполагаемыми обработчиками
+// (будут меняться после создания моделей)
+
 import { Request, Response } from 'express';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import db from './models';
+
+const { Topic, Author, Message } = db;
+
+// Создаёт новый топик
 const createTopic = (req: Request, res: Response) => {
-  if (!req.body.body) {
+  if (!req.body.title) {
     res.statusCode = 400;
-    res.send({ 'error': 'The feedback field is required' });
+    res.send('The title field is required');
 
     return;
   }
 
-  if (!req.body.userId) {
-    res.statusCode = 400;
-    res.send({ 'error': 'userId is required' });
+  const topic = {
+    title: req.body.title,
+    author: req.body.userId,
+  };
 
-    return;
-  }
+  const author = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    id: req.body.userId
+  };
 
-  res.send('Ok');
+  Topic.create(topic)
+    .then(() => {
+      Author.create(author)
+        .then(() => {
+          res.send('Ok');
+        });
+    })
+    .catch(err => {
+      res.status(500).send(
+        err.message || 'Some error occurred while creating topic.'
+      );
+    });
 };
 
+// Берёт и отдаёт все топики
 const getTopics = (req: Request, res: Response) => {
-  console.log(req, res);
+  // Тут пока хз как авторов брать, будет понятнее когда будут модели
+  Topic.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(
+        err.message || 'Some error occurred while retrieving topics.'
+      );
+    });
 };
 
-const createMessage = (req: Request, res: Response) => {
-  console.log(req, res);
-};
-
-const getMessages = (req: Request, res: Response) => {
-  console.log(req, res);
-};
-
+// Берёт конкретный топик
 const getTopic = (req: Request, res: Response) => {
-  console.log(req, res);
+  // Пока так же нет получения автора
+  const topicId = req.params.id;
+
+  Topic.findByPk(topicId)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(() => {
+      res.status(500).send('Error retrieving Topic with id ' + topicId);
+    });
+};
+
+// Создаёт сообщение в конкретном топике
+const createMessage = (req: Request, res: Response) => {
+  if (!req.body.message) {
+    res.statusCode = 400;
+    res.send('The message field is required');
+
+    return;
+  }
+
+  const message = {
+    title: req.body.message,
+    author: req.body.userId,
+    topicId: req.params.id
+  };
+
+  const author = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    id: req.body.userId
+  };
+
+  Message.create(message)
+    .then(() => {
+      Author.create(author)
+        .then(() => {
+          res.send('Ok');
+        });
+    })
+    .catch(err => {
+      res.status(500).send(
+        err.message || 'Some error occurred while creating message.'
+      );
+    });
+};
+
+// Отдаёт сообщения для конкретного топика
+const getMessages = (req: Request, res: Response) => {
+  const topicId = req.params.id;
+
+  Message.findAll({ where: topicId })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send(
+        err.message || 'Some error occurred while retrieving messages.'
+      );
+    });
 };
 
 export {
