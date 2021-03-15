3a902e513e3f28aa1d2de78185638bdb237dc2a1
Anatoliy Bartsev <a.barczev@yandex.ru>
2021-03-15 08:54:57 +0300

    72 add api feedback (#66)
    * добавил поле в форму
    
    * добавил апи на форму
    
    * удалил title

--- src/app/feedback/models/feedback.ts
+++ src/app/feedback/models/feedback.ts
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 
 const feedbackSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  title: String,
-  body: {
+  message: {
     type: String,
     required: true,
   },
--- src/pages/feedback/feedback.tsx
+++ src/pages/feedback/feedback.tsx
@@ -7,47 +7,37 @@ import { useForm } from '../../hooks';
 
 import styles from '../sign-in-up.pcss';
 
-const signInFields = [
-  {
-    name: 'massage',
-    label: 'Сообщение',
-  },
-];
-
 export const Feedback = withStyles(styles)(() => {
-  const requiredFields = ['massage'];
+  const requiredFields = ['message'];
 
   const {
-    handleSubmitSign,
+    handleSubmitFeedback,
     handleChange,
     handleBlur,
     error,
     fields: fieldsValues
-  } = useForm({ requiredFields, url: API_URL.FEEDBACK });
+  } = useForm({ requiredFields, url: API_URL.FEEDBACK }, '/');
 
   return (
     <div className={styles.page}>
       <div className={styles.content}>
         <Form
-          onSubmit={handleSubmitSign}
+          onSubmit={handleSubmitFeedback}
           submitButtonText={APP_TEXT.BTN_SEND}
           title={'Обратная связь'}
         >
-          {signInFields.map((field) => (
-            <Field
-              key={field.name}
-              label={field.label}
-              error={error[field.name]}
-            >
-              <Textarea
-                name={field.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={error[field.name]}
-                value={fieldsValues[field.name] || ''}
-              />
-            </Field>
-          ))}
+          <Field
+            label={'Сообщение'}
+            error={error['message']}
+          >
+            <Textarea
+              name={'message'}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={error['message']}
+              value={fieldsValues['message'] || ''}
+            />
+          </Field>
         </Form>
       </div>
     </div>
