2d478270ac3d165e5f061a59f444424584c87bcb
Aleksandr Kochura <kochura@gmail.com>
2021-03-20 00:13:44 +0300

    миграция для форума

---
+++ src/server/migrations/20210319205242-forum.js
@@ -0,0 +1,66 @@
+'use strict';
+
+import {DataType} from 'sequelize-typescript';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('author', {
+      firstName: Sequelize.STRING,
+      lastName: Sequelize.STRING,
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.createTable('topic', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      title: Sequelize.STRING(1000),
+      AuthorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Author',
+          key: 'id',
+        }
+      },
+    });
+
+    await queryInterface.createTable('message', {
+      message: DataType.STRING(5000),
+      id: {
+        type: DataType.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      firstName: Sequelize.STRING,
+      lastName: Sequelize.STRING,
+      AuthorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Author',
+          key: 'id',
+        }
+      },
+      TopicId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Topic',
+          key: 'id',
+        }
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('topic');
+    await queryInterface.dropTable('author');
+    await queryInterface.dropTable('message');
+  }
+};
